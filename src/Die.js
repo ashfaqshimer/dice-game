4344d8a0298073d8e33bd5933d998272a06ba259
Ashfaq Shimer <shimer.ahamed@gmail.com>
2019-06-05 00:02:04 +0530

    Get dice faces from defaultProps. Add animation

--- src/Die.js
+++ src/Die.js
@@ -4,37 +4,10 @@ import './Die.css';
 
 export default class Die extends Component {
 	render() {
-		let dieNumber = null;
-		const { number } = this.props;
-
-		switch (number) {
-			case 1:
-				dieNumber = 'fa-dice-one';
-				break;
-			case 2:
-				dieNumber = 'fa-dice-two';
-				break;
-			case 3:
-				dieNumber = 'fa-dice-three';
-				break;
-			case 4:
-				dieNumber = 'fa-dice-four';
-				break;
-			case 5:
-				dieNumber = 'fa-dice-five';
-				break;
-			case 6:
-				dieNumber = 'fa-dice-six';
-				break;
-
-			default:
-				dieNumber = 'fa-dice-six';
-				break;
-		}
-
+		const { number, rolling } = this.props;
 		return (
 			<div className='Die'>
-				<i className={`fas ${dieNumber} fa-7x`} />
+				<i className={`fas fa-dice-${number} fa-7x ${rolling && 'shaking'}`} />
 			</div>
 		);
 	}
--- src/RollDice.js
+++ src/RollDice.js
@@ -5,25 +5,30 @@ import './RollDice.css';
 export default class RollDice extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { number1: 6, number2: 6, rolling: false };
+		this.state = { number1: 'six', number2: 'six', rolling: false };
 		this.roll = this.roll.bind(this);
 	}
 
+	static defaultProps = {
+		dice: ['one', 'two', 'three', 'four', 'five', 'six']
+	};
+
 	roll() {
-		const random1 = Math.floor(Math.random() * 10) + 1;
-		const random2 = Math.floor(Math.random() * 10) + 1;
+		const random1 = this.props.dice[Math.floor(Math.random() * this.props.dice.length)];
+		const random2 = this.props.dice[Math.floor(Math.random() * this.props.dice.length)];
+
 		this.setState({ rolling: true });
 		setTimeout(() => {
 			this.setState({ number1: random1, number2: random2, rolling: false });
-		}, 2000);
+		}, 1000);
 	}
 
 	render() {
 		return (
 			<div className='RollDice'>
 				<h1 className='RollDice-title'>Winner Winner, Chicken Dinner!</h1>
-				<Die number={this.state.number1} />
-				<Die number={this.state.number2} />
+				<Die number={this.state.number1} rolling={this.state.rolling} />
+				<Die number={this.state.number2} rolling={this.state.rolling} />
 				<button className='RollDice-button' disabled={this.state.rolling} onClick={this.roll}>
 					{this.state.rolling ? 'Rolling...' : 'Roll Dice!'}
 				</button>
